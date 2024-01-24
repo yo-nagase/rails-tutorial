89d5147e6ead8c0c367bf70f3f1ce6c1f56f3758
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2024-01-24 22:47:58 +0900

    参照、新規、一覧機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # get "/xxx", to: "articles#xxx"
+
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
